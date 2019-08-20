3f63ea6764a34dbc9d17e4457924940c1c75ffeb
Pho Hale <halechr@umich.edu>
2019-08-20 18:14:27 -0400

    Created a cleaner config.h file by importing the port settings from individual files in the "ConfigFiles" folder.

---
+++ ConfigFiles/config-DownstairsBox.h
@@ -0,0 +1,17 @@
+#pragma once
+//config-DownstairsBox.h:
+/*
+Specifies what ports to open. Imported by config.h.
+*/
+
+
+//// Inputs:
+// Analog Breakout Board:
+enum { NUM_CHANNELS = 9 };
+#define globalLabjackInputPortNames { "DIO0","DIO1","DIO2","DIO3","DIO4","DIO5","DIO6","DIO7","MIO0" }
+#define globalLabjackInputPortPurpose { "Water1_BeamBreak","Water2_BeamBreak","Food1_BeamBreak","Food2_BeamBreak","Water1_Dispense","Water2_Dispense","Food1_Dispense","Food2_Dispense","RunningWheel_Beambreak"}
+
+//// Outputs:
+enum { NUM_OUTPUT_CHANNELS = 3 };
+#define globalLabjackOutputPortNames { "MIO2","MIO1","MIO1" }
+#define globalLabjackInputPortPurpose { "VisibleLedRelay","WaterPortAttractModeLED","WaterPortAttractModeLED"}
---
+++ ConfigFiles/config-UpstairsBox-8-20-2019.h
@@ -0,0 +1,17 @@
+#pragma once
+//config-UpstairsBox-8-20-2019.h:
+/*
+Specifies what ports to open. Imported by config.h.
+*/
+
+
+//// Inputs:
+// Digital Breakout Board:
+enum { NUM_CHANNELS = 9 };
+#define globalLabjackInputPortNames { "EIO0","EIO1","EIO2","EIO3","EIO4","EIO5","EIO6","EIO7","CIO0"}
+#define globalLabjackInputPortPurpose { "Water1_BeamBreak","Water2_BeamBreak","Food1_BeamBreak","Food2_BeamBreak","Water1_Dispense","Water2_Dispense","Food1_Dispense","Food2_Dispense","RunningWheel_Beambreak"}
+
+//// Outputs:
+enum { NUM_OUTPUT_CHANNELS = 3 };
+#define globalLabjackOutputPortNames { "CIO1","CIO2","CIO2" }
+#define globalLabjackInputPortPurpose { "VisibleLedRelay","WaterPortAttractModeLED","WaterPortAttractModeLED"}
