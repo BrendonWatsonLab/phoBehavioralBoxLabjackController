b22d24e408f9cbe0df3676c21315c7f0bade793e
Pho Hale <halechr@umich.edu>
2019-08-22 13:30:49 -0400

    Rough implementation of a controller singleton

--- src/BehavioralBoxLajbackController.cpp
+++ src/BehavioralBoxLajbackController.cpp
@@ -1,1 +1,63 @@
+#include <Wt/WApplication.h>
+#include <Wt/WServer.h>
 #include "BehavioralBoxLajbackController.h"
+
+BehavioralBoxLajbackController::BehavioralBoxLajbackController()
+{
+	this->thread_ = std::thread(std::bind(&BehavioralBoxLajbackController::run, this));
+}
+
+BehavioralBoxLajbackController::~BehavioralBoxLajbackController()
+{
+	this->shouldStop_ = true;
+	this->thread_.join();
+}
+
+void BehavioralBoxLajbackController::connect(Client* client, const std::function<void()>& function)
+{
+	std::unique_lock<std::mutex> lock(mutex_);
+	connections_.push_back(Connection(Wt::WApplication::instance()->sessionId(), client, function));
+}
+
+void BehavioralBoxLajbackController::disconnect(Client* client)
+{
+	std::unique_lock<std::mutex> lock(mutex_);
+
+	for (unsigned i = 0; i < connections_.size(); ++i) {
+		if (connections_[i].client == client) {
+			connections_.erase(connections_.begin() + i);
+			return;
+		}
+	}
+	assert(false);
+}
+
+void BehavioralBoxLajbackController::run()
+{
+	/*
+   * This method simulates changes to the data that happen in a background
+   * thread.
+   */
+	for (;;) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+		if (this->shouldStop_) {
+			return;
+		}
+
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			//++counter_;
+			//TODO: Update the number of labjacks and such
+			// **HERE**
+
+
+			// **HERE**
+			/* This is where we notify all connected clients. */
+			for (unsigned i = 0; i < connections_.size(); ++i) {
+				Connection& c = connections_[i];
+				Wt::WServer::instance()->post(c.sessionId, c.function);
+			}
+		}
+	}
+}
--- src/BehavioralBoxLajbackController.h
+++ src/BehavioralBoxLajbackController.h
@@ -1,5 +1,70 @@
 #pragma once
+
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+#include "BehavioralBoxLabjack.h"
+
+/*
+ * Simple interface to uniquely identify a client
+ */
+class Client {
+};
+
+
+/*
+ * A (singleton) server class which would protect and manage a shared
+ * resource of a vector of Labjacks. In our example we take a simple counter as data.
+ */
 class BehavioralBoxLajbackController
 {
+public:
+	BehavioralBoxLajbackController();
+	~BehavioralBoxLajbackController();
+
+
+
+	// Server Stuff:
+	void connect(Client* client, const std::function<void()>& function);
+	void disconnect(Client* client);
+
+	std::vector<BehavioralBoxLabjack*> getActiveLabjacks() const {
+		std::unique_lock<std::mutex> lock(mutex_);
+		return this->foundLabjacks_;
+	}
+
+	// Shutsdown the server
+	void shutdown() {
+		this->shouldStop_ = true;
+	}
+
+	//bool waitForFoundLabjacks();
+	//int shutdownApplication(int shutdownCode);
+
+private:
+	struct Connection {
+		Connection(const std::string& id, Client* c,
+			const std::function<void()>& f)
+			: sessionId(id),
+			client(c),
+			function(f)
+		{ }
+
+		std::string sessionId;
+		Client* client;
+		std::function<void()> function;
+	};
+
+	mutable std::mutex mutex_;
+	std::thread thread_;
+	// Vector of Labjack Objects
+	std::vector<BehavioralBoxLabjack*> foundLabjacks_;
+	bool shouldStop_ = false;
+
+	std::vector<Connection> connections_;
+
+	void run();
 };
 
