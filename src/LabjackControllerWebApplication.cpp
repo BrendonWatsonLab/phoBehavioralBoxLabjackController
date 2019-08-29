81d6003938e9ca48be5b046038fb7d13d4a351ae
Pho Hale <halechr@umich.edu>
2019-08-29 14:25:03 -0400

    Added loading indicator

--- src/LabjackControllerWebApplication.cpp
+++ src/LabjackControllerWebApplication.cpp
@@ -1,4 +1,5 @@
 #include <Wt/WApplication.h>
+#include <Wt/WOverlayLoadingIndicator.h>
 #include "LabjackControllerWebApplication.h"
 #include "LabjackHelpers.h"
 
@@ -17,7 +18,13 @@ LabjackControllerWebApplication::LabjackControllerWebApplication(const WEnvironm
 	root()->setPadding(10);
 	root()->resize(WLength::Auto, WLength::Auto);
 
+	//// Loading Indicator
+	//auto loadingIndicatorWidget = root()->addWidget(cpp14::make_unique<WOverlayLoadingIndicator>());
+	//loadingIndicatorWidget->setMessage("Loading History, please wait...");
+
+	// Main Widget
 	this->labjackControllerOuterWidget = root()->addWidget(cpp14::make_unique<LabjackControllerOuterWidget>());
+
 
 	/*
 	 * Set our style sheet last, so that it loaded after the ext stylesheets.
@@ -41,6 +48,12 @@ void LabjackControllerWebApplication::staticRefreshLabjacksData()
 
 void LabjackControllerWebApplication::updateActiveLabjacks(std::vector<BehavioralBoxLabjack*> updatedLabjacks)
 {
+	// Loading Indicator
+	//auto loadingIndicatorWidget = root()->addWidget(cpp14::make_unique<WOverlayLoadingIndicator>());
+	//loadingIndicatorWidget->setMessage("Loading History, please wait...");
+	this->setLoadingIndicator(cpp14::make_unique<WOverlayLoadingIndicator>());
+
+
 	if (this->labjackControllerOuterWidget != NULL) {
 		this->labjackControllerOuterWidget->setActiveLabjacks(updatedLabjacks);
 		WApplication::instance()->triggerUpdate();
