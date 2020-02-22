de7170f70849eb6abe3a3e1936b17f6b0c27dc41
Pho Hale <PhoHale@gmail.com>
2020-02-21 20:10:32 -0500

    pending script

---
+++ Installer/Copy-Installer-To-Deployables.cmd
@@ -0,0 +1,3 @@
+
+"C:\Common\repo\phoBehavioralBoxLabjackController\Installer\Install phoBehavioralBoxLabjackController.exe"
+"C:\BehavioralBoxServerShare\Behavioral Box Computer Setup - 9-5-2019\Combined Installer\Combined Installer\4 - Auto\Install phoBehavioralBoxLabjackController.exe"
