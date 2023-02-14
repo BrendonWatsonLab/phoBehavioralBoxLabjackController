f00892cf6a1b86363085631fbe10daf01e55c07e
Noah Muscat <noahmu@umich.edu>
2023-02-14 13:23:14 -0500

    Cut out server and historical data from controllers manager
    Main concern is if historical data really means the data gathered from labjack. If that's the case, all of these cuts will have to be undone.

--- src/BehavioralBoxControllersManager.cpp
+++ src/BehavioralBoxControllersManager.cpp
@@ -17,34 +17,12 @@ BehavioralBoxControllersManager::BehavioralBoxControllersManager()
 
 BehavioralBoxControllersManager::~BehavioralBoxControllersManager()
 {
-	this->shouldStop_ = true;
+    this->shouldStop_ = true;
 	// As the thread is using members from this object
 	  // We can not let this obect be destroyed until
 	  // the thread finishes executing.
-	this->thread_reloadHistoricalData_.join();
 	this->thread_.join();
 
-}
-
-void BehavioralBoxControllersManager::connect(Client* client, const std::function<void()>& function)
-{
-	std::unique_lock<std::mutex> lock(mutex_);
-#if LAUNCH_WEB_SERVER
-	connections_.push_back(Connection(Wt::WApplication::instance()->sessionId(), client, function));
-#endif // LAUNCH_WEB_SERVER
-}
-
-void BehavioralBoxControllersManager::disconnect(Client* client)
-{
-	std::unique_lock<std::mutex> lock(mutex_);
-
-	for (unsigned i = 0; i < connections_.size(); ++i) {
-		if (connections_[i].client == client) {
-			connections_.erase(connections_.begin() + i);
-			return;
-		}
-	}
-	assert(false);
 }
 
 bool BehavioralBoxControllersManager::scanForNewLabjacks()
@@ -173,135 +151,7 @@ void BehavioralBoxControllersManager::run()
 			//TODO: Update the number of labjacks and such
 			// **HERE**
 
-
-			// **HERE**
-			/* This is where we notify all connected clients. */
-			for (unsigned i = 0; i < connections_.size(); ++i) {
-				Connection& c = connections_[i];
-#if LAUNCH_WEB_SERVER
-				Wt::WServer::instance()->post(c.sessionId, c.function);
-#endif // LAUNCH_WEB_SERVER
-
-				
 			}
 		}
 	}
-}
-
-
-//TODO: currently doesn't make use of the individual Labjack object's output directory, and instead uses this class' member variable dataFilesSearchDirectory_
-void BehavioralBoxControllersManager::reloadHistoricalData()
-{
-	if (!this->configMan->getLoadedConfig().enableHistoricalDataLoading) {
-		// Historical Data loading is disabled, just return.
-		//TODO: validate that this is acceptable.
-		return;
-	}
-	//TODO: do on background thread
-	// Find the folders to search:
-
-	fs::path curr_search_dir = this->dataFilesSearchDirectory_;
-	// Get the children search dirs:
-	std::map<int, fs::path> foundBoxPaths = FilesystemHelpers::findBehavioralBoxDataFolders(curr_search_dir);
-	//std::vector<int> found_box_ids;
-
-	// Get the maximum found box ID from the found paths:
-	int maximum_found_box_id = -1;
-	for (std::map<int, fs::path>::iterator it = foundBoxPaths.begin(); it != foundBoxPaths.end(); ++it) {
-		maximum_found_box_id = (std::max)(maximum_found_box_id, it->first);
-		//found_box_ids.push_back(it->first);
-		//std::cout << it->first << "\n";
-	}
-	std::cout << "Maximum found box id is " << maximum_found_box_id << "." << std::endl;
-
-	//maximum_found_box_id
-	this->behavioralBoxEventDataFilesMap_ = std::map<int, std::vector<LabjackDataFile>>();
-	// Iterate through the BBIDs to initialize the vectors
-	for (size_t i = 0; i < maximum_found_box_id; i++)
-	{
-		int curr_box_id = i + 1;
-		this->behavioralBoxEventDataFilesMap_[curr_box_id] = std::vector<LabjackDataFile>();
-	}
-
-	///Finds the correct animal folder within each found BB folder
-	std::map<int, fs::path> foundSearchPaths = FilesystemHelpers::findActiveExperimentAnimalFolders(curr_search_dir);
-	for (const auto& activeBoxSearchPathPair : foundSearchPaths) {
-		int curr_bbID = activeBoxSearchPathPair.first;
-		fs::path curr_search_path = activeBoxSearchPathPair.second;
-		// Search that path for files:
-		this->behavioralBoxEventDataFilesMap_[curr_bbID] = FilesystemHelpers::findAllDataFiles(curr_search_path.string());
-
-	}
-
-	//TODO: change to sort by search paths, not labjack serials
-	//labjackDataFilesMap_ = FilesystemHelpers::findDataFiles(this->dataFilesSearchDirectory_);
-
-	if (this->shouldStop_) {
-		// Not ready.
-		return;
-	}
-	// Clear the old historical data
-	this->historicalData_.clear();
-	// Iterate through the search paths one more time and build historical data objects (BehavioralBoxHistoricalData) from the found data files
-	for (const auto& activeBoxSearchPathPair : foundSearchPaths) {
-		int curr_bbID = activeBoxSearchPathPair.first;
-		std::string curr_bbID_String = FormattingHelper::format_two_digit_string(curr_bbID);
-		fs::path curr_search_path = activeBoxSearchPathPair.second;
-		BehavioralBoxHistoricalData currHistoryData = BehavioralBoxHistoricalData(curr_search_path.string(), -1, curr_bbID_String, this->behavioralBoxEventDataFilesMap_[curr_bbID]);
-		this->historicalData_.push_back(currHistoryData);
-	}
-
-	//if (this->getActiveLabjacks().size() > 0) {
-	//	// Loop through the active labjacks and get the historical data corresponding to them.
-	//	for (int i = 0; i < this->getActiveLabjacks().size(); i++) {
-	//		BehavioralBoxHistoricalData currHistoryData = this->getHistoricalData(this->getActiveLabjacks()[i]->getSerialNumber());
-	//		this->historicalData_.push_back(currHistoryData);
-	//	}
-	//}
-	//else {
-	//	// No actual labjacks, load **everything**:
-	//	//this->historicalData_ = BehavioralBoxControllersManager::loadAllHistoricalData();
-	//	for (const auto& bbFilesPair : this->behavioralBoxEventDataFilesMap_) {
-	//		int curr_bbID = bbFilesPair.first;
-	//		std::string curr_bbID_String = FormattingHelper::format_two_digit_string(curr_bbID);
-
-
-	//		BehavioralBoxHistoricalData currHistoryData = BehavioralBoxHistoricalData(this->dataFilesSearchDirectory_, labjackFilesPair.first, labjackFilesPair.second);
-	//		this->historicalData_.push_back(currHistoryData);
-	//	}
-	//}
-
-}
-
-std::vector<std::string> BehavioralBoxControllersManager::exportHistoricalDataAsCSV(std::string path, std::string base_filename)
-{
-	std::string fullPrefixPath = path + base_filename + "_";
-	std::vector<std::string> outputPaths;
-	for (int i = 0; i < this->historicalData_.size(); i++)
-	{
-		//TODO: this exports all historical data to the same file... is this okay?
-		std::string currFullOutputPath = fullPrefixPath + std::to_string(i) + ".csv";
-		outputPaths.push_back(currFullOutputPath);
-		this->historicalData_[i].exportAsCSV(currFullOutputPath);
-	}
-	return outputPaths;
-}
-
-
-void BehavioralBoxControllersManager::serverGetAllHistoricalData(HistoricalDataLoadingEventCallback completionCallback)
-{
-	auto updatedData = HistoricalDataLoadingEvent(this->historicalData_);
-	completionCallback(updatedData);
-}
-
-// called by the BoxControllerWebDataServer to request updated historical data
-void BehavioralBoxControllersManager::serverLoadAllHistoricalData(HistoricalDataLoadingEventCallback completionCallback)
-{
-	// std::lock_guard
-	this->thread_reloadHistoricalData_ = std::move(std::thread([=]() {
-		this->reloadHistoricalData();
-		auto updatedData = HistoricalDataLoadingEvent(this->historicalData_);
-		completionCallback(updatedData);
-		return 1;
-	}));
 }
--- src/BehavioralBoxControllersManager.h
+++ src/BehavioralBoxControllersManager.h
@@ -29,18 +29,9 @@ public:
 	BehavioralBoxControllersManager();
 	~BehavioralBoxControllersManager();
 
-	// Server Stuff:
-	void connect(Client* client, const std::function<void()>& function);
-	void disconnect(Client* client);
-
 	std::vector<BehavioralBoxLabjack*> getActiveLabjacks() const {
 		std::unique_lock<std::mutex> lock(mutex_);
 		return this->foundLabjacks_;
-	}
-
-	// Shutsdown the server
-	void shutdown() {
-		this->shouldStop_ = true;
 	}
 
 	// Scans for un-managed Labjacks:
@@ -50,19 +41,6 @@ public:
 	bool waitForFoundLabjacks();
 	//int shutdownApplication(int shutdownCode);
 
-	std::vector<BehavioralBoxHistoricalData> getLoadedHistoricalData() { return this->historicalData_; }
-	void reloadHistoricalData();
-
-	// exports the historical data to individual .CSV files and returns the output paths.
-	std::vector<std::string> exportHistoricalDataAsCSV(std::string path, std::string base_filename);
-
-	// Server/Manager:
-	//void postHistoricalDataUpdatedEvent(const HistoricalDataLoadingEvent& event);
-	//serverGetAllHistoricalData(...): gets the latest version of the historical data
-	void serverGetAllHistoricalData(HistoricalDataLoadingEventCallback completionCallback);
-	//serverLoadAllHistoricalData(...): reloads all historical data and then gets it
-	void serverLoadAllHistoricalData(HistoricalDataLoadingEventCallback completionCallback);
-
 	// Getters:
 	bool isReady();
 	// Gets the computer's hostname
@@ -70,26 +48,11 @@ public:
 	// Gets the 2-digit integer identifier for the current computer (and box, if there is a 1-to-1 mapping). Like the "02" in "WATSON-BB-02"
 	int getNumericComputerIdentifier() { return this->numeric_computer_identifer_; }
 	
-
-
 private:
-	struct Connection {
-		Connection(const std::string& id, Client* c,
-			const std::function<void()>& f)
-			: sessionId(id),
-			client(c),
-			function(f)
-		{ }
-
-		std::string sessionId;
-		Client* client;
-		std::function<void()> function;
-	};
 
 	// State Variables:
 	mutable std::mutex mutex_;
 	std::thread thread_;
-	std::thread thread_reloadHistoricalData_; // used for reloading the historical data
 	bool stillWaitingToFindLabjacks_ = false;
 	bool shouldStop_ = false;
 
@@ -103,24 +66,14 @@ private:
 	// CSV Export testing variables
 	const bool wants_export_to_csv_on_reload = false;
 	std::string csvExportPath = "C:/Common/data/export-HistoricalData.csv";
-	
-	std::vector<Connection> connections_;
 
 	void run();
-
-	//Historical Labjack Data loading:
-	std::shared_ptr<ConfigurationManager> configMan = std::make_shared<ConfigurationManager>();
-	std::string hostName_ = configMan->getHostName();
-	int numeric_computer_identifer_ = configMan->getNumericComputerIdentifier();
-	std::string dataFilesSearchDirectory_ = configMan->getGeneratedActiveHistoricalSearchDirectory();
 
 	// behavioralBoxEventDataFilesMap_: indexed by the BBID (1-indexed)
 	std::map<int, std::vector<LabjackDataFile>> behavioralBoxEventDataFilesMap_;
 	// TODO: replace labjackDataFilesMap_
 	//std::map<int, std::vector<LabjackDataFile>> labjackDataFilesMap_;
 	
-	std::vector<BehavioralBoxHistoricalData> historicalData_;
-
 	//TODO: currently doesn't make use of the individual Labjack object's output directory, and instead uses this class' member variable dataFilesSearchDirectory_
 	//BehavioralBoxHistoricalData getHistoricalData(int labjackSerialNumber, unsigned long long startMillisecondsSinceEpoch, unsigned long long endMillisecondsSinceEpoch);
 	//BehavioralBoxHistoricalData getHistoricalData(int labjackSerialNumber);
